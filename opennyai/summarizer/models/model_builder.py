679d060321f1f1f1d834e92fb94a4a851a6a40f1
Flyboy990 <86570970+Flyboy990@users.noreply.github.com>
2025-10-07 22:20:55 +0530

    Fix strict=False for position_ids compatibility

--- opennyai/summarizer/models/model_builder.py
+++ opennyai/summarizer/models/model_builder.py
@@ -173,7 +173,7 @@ class ExtSummarizer(nn.Module):
             self.bert.model.embeddings.position_embeddings = my_pos_embeddings
 
         if checkpoint is not None:
-            self.load_state_dict(checkpoint['model'], strict=True)
+            self.load_state_dict(checkpoint['model'], strict=False)
         else:
             if args.param_init != 0.0:
                 for p in self.ext_layer.parameters():
